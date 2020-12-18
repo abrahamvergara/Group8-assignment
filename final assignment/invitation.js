ed2b786584e37f7f7e05349f348a88087e768cb0
MartinAngelo <74279352+MartinAngelo@users.noreply.github.com>
2020-12-18 17:36:55 +0800

    final invitation

--- final assignment/invitation.js
+++ final assignment/invitation.js
@@ -1,19 +1,34 @@
-window.onload = function() {
+window.onload = function () {
 
     var modal = document.getElementById("myModal");
+    var modal2 = document.getElementById("myModal2");
     var btn = document.getElementById("myBtn");
+    var btn2 = document.getElementById("myBtn2");
     var span = document.getElementsByClassName("close")[0];
+    var span2 = document.getElementsByClassName("close2")[0];
 
 
 
-    btn.onclick = function() {
+    btn.onclick = function () {
         modal.style.display = "block";
     }
-    span.onclick = function() {
+    span.onclick = function () {
         modal.style.display = "none";
     }
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    btn2.onclick = function() {
+        modal.style.display = "block";
+    }
+    span2.onclick = function() {
+        modal2.style.display = "none";
+    }
+    window2.onclick = function(event) {
+        if (event.target == modal2) {
             modal.style.display = "none";
         }
 
